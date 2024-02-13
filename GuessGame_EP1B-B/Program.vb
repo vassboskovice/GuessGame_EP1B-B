980d38039907f35915eaf6621c7bd73c7158ec1b
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2024-02-13 09:01:56 +0100

    Metoda Menu

--- GuessGame_EP1B-B/Program.vb
+++ GuessGame_EP1B-B/Program.vb
@@ -1,7 +1,24 @@
-Imports System
+﻿Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+        Console.CursorVisible = False
+
+        Menu()
+
+        Console.ReadKey()
+    End Sub
+
+    Sub Menu()
+        Console.WriteLine("╔═════════════════════════╗")
+        Console.WriteLine("║       HÁDÁNÍ ČÍSEL      ║")
+        Console.WriteLine("╠═════════════════════════╣")
+        Console.WriteLine("║ 1 - Číslo hádá uživatel ║")
+        Console.WriteLine("║ 2 - Číslo hádá počítač  ║")
+        Console.WriteLine("╟─────────────────────────╢")
+        Console.WriteLine("║ Esc - Zavřít aplikaci   ║")
+        Console.WriteLine("╠═════════════════════════╣")
+        Console.WriteLine("║   Vyber položku menu    ║")
+        Console.WriteLine("╚═════════════════════════╝")
     End Sub
 End Module
