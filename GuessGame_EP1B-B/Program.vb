77421d56d97cabe0f25dbd6bba36cec291960c6a
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2024-02-13 09:39:51 +0100

    Funkční menu

--- GuessGame_EP1B-B/Program.vb
+++ GuessGame_EP1B-B/Program.vb
@@ -5,9 +5,19 @@ Module Program
         Console.CursorVisible = False
         Console.BackgroundColor = ConsoleColor.White
         Console.ForegroundColor = ConsoleColor.Black
-        Menu()
-
-        Console.ReadKey()
+        Dim klavesa As ConsoleKeyInfo
+        Do
+            Console.Clear()
+            Menu()
+            klavesa = Console.ReadKey(True)
+            If klavesa.KeyChar = "1" Then
+                HadaUzivatel()
+            ElseIf klavesa.KeyChar = "2" Then
+                HadaPocitac()
+            ElseIf klavesa.Key = ConsoleKey.Escape Then
+                Exit Do
+            End If
+        Loop
     End Sub
 
     Sub Menu()
@@ -16,10 +26,21 @@ Module Program
         Console.WriteLine("╠═════════════════════════╣")
         Console.WriteLine("║ 1 - Číslo hádá uživatel ║")
         Console.WriteLine("║ 2 - Číslo hádá počítač  ║")
-        Console.WriteLine("╟─────────────────────────╢")
         Console.WriteLine("║ Esc - Zavřít aplikaci   ║")
         Console.WriteLine("╠═════════════════════════╣")
         Console.WriteLine("║   Vyber položku menu    ║")
         Console.WriteLine("╚═════════════════════════╝")
+    End Sub
+
+    Sub HadaUzivatel()
+        Console.Clear()
+        Console.WriteLine("1 - Číslo hádá uživatel")
+        Console.ReadKey()
+    End Sub
+
+    Sub HadaPocitac()
+        Console.Clear()
+        Console.WriteLine("2 - Číslo hádá počítač")
+        Console.ReadKey()
     End Sub
 End Module
