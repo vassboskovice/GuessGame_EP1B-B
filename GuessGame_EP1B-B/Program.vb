cfd946a1119c4026b581e62a4396dde5e8b85b13
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2024-02-20 09:36:37 +0100

    HOTOVO

--- GuessGame_EP1B-B/Program.vb
+++ GuessGame_EP1B-B/Program.vb
@@ -33,14 +33,70 @@ Module Program
     End Sub
 
     Sub HadaUzivatel()
+        Dim cislo, tip As Integer
+        Dim vstup As String
+        Dim random As Random = New Random 'generátor náhodných čísel
+
+        cislo = random.Next(0, 1000) 'náhodné číslo v rozmezí 0 - 999
+
         Console.Clear()
-        Console.WriteLine("1 - Číslo hádá uživatel")
+        Console.WriteLine(" 1 - Číslo hádá uživatel")
+        Console.WriteLine("---------------------------------------")
+        Do
+            Console.Write(" Tipni si číslo (0 - 999): ")
+            vstup = Console.ReadLine
+            If Integer.TryParse(vstup, tip) Then
+                If tip = cislo Then
+                    Console.WriteLine(" Výborně, zásah!")
+                    Exit Do
+                ElseIf tip < cislo Then
+                    Console.WriteLine(" To je málo...")
+                Else
+                    Console.WriteLine(" To je moc...")
+                End If
+            Else
+                Console.WriteLine(" Musíš zadat celé číslo!!")
+            End If
+        Loop
+        Console.WriteLine("---------------------------------------")
+        Console.WriteLine("Díky za hru, pro návrat do menu stiskni libovolnou klávesu...")
         Console.ReadKey()
     End Sub
 
     Sub HadaPocitac()
+        Dim min, max, tip As Integer
+        Dim vstup As String
+        Dim klavesa As ConsoleKeyInfo
+
         Console.Clear()
         Console.WriteLine("2 - Číslo hádá počítač")
+        Console.WriteLine("---------------------------------------")
+        Console.Write(" Zadej minimum: ")
+        vstup = Console.ReadLine
+        If Integer.TryParse(vstup, min) Then
+            Console.Write(" Zadej maximum: ")
+            vstup = Console.ReadLine
+            If Integer.TryParse(vstup, max) Then
+                Console.WriteLine("---------------------------------------")
+                Do
+                    tip = (min + max) / 2
+                    Console.WriteLine($" Je to {tip}? (1 - málo; 2 - moc; jinak - zásah)")
+                    klavesa = Console.ReadKey
+                    If klavesa.KeyChar = "1" Then
+                        min = tip
+                    ElseIf klavesa.KeyChar = "2" Then
+                        max = tip
+                    Else
+                        Console.WriteLine(" Skvělé, perfektně jsme si zahráli. Pro návrat stiskni libovolnou klávesu...")
+                        Exit Do
+                    End If
+                Loop
+            Else
+                Console.WriteLine(" Musíš zadat celé číslo!!")
+            End If
+        Else
+            Console.WriteLine(" Musíš zadat celé číslo!!")
+        End If
         Console.ReadKey()
     End Sub
 End Module
